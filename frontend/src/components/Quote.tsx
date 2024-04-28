9ba88ec5cc631a0683a3deb13e375ed0b6344654
imaditya05 <imaditya05@gmail.com>
2024-04-28 23:39:44 +0530

    Signup Page frontend

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,9 @@
+export const Quote = () => {
+  return (
+    <div className="bg-slate-200 h-screen flex flex-col justify-center">
+      <div className="flex justify-center">
+        <div className=" max-w-md text-center">Hi quote</div>
+      </div>
+    </div>
+  );
+};
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,5 +1,10 @@
+import { Quote } from "../components/Quote";
+
 export function Signup(){
-    return <>
-    Signup
-    </>
+    return <div className="grid grid-cols-2">
+        <div></div>
+    <Quote />
+    </div>
+    
+
 }
